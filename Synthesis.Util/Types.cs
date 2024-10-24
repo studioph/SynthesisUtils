5b50a9bf9a48062711a9fadd78a6261f242daff7
Paul Hutchings <dev@studiop.page>
2024-10-24 15:22:52 -0400

    Add alternate constructor extension methods

--- Synthesis.Util/Types.cs
+++ Synthesis.Util/Types.cs
@@ -1,3 +1,4 @@
+using Mutagen.Bethesda;
 using Mutagen.Bethesda.Plugins.Cache;
 using Mutagen.Bethesda.Plugins.Records;
 
@@ -40,4 +41,41 @@ namespace Synthesis.Util
         where TMajor : TMajorGetter
         where TMajorGetter : IMajorRecordQueryableGetter
         where TValue : notnull;
+
+    public static class ConstructorExtensions
+    {
+        public static ForwardRecordContext<TMod, TModGetter, TMajor, TMajorGetter> WithContext<
+            TMod,
+            TModGetter,
+            TMajor,
+            TMajorGetter
+        >(this TMajorGetter record, ILinkCache<TMod, TModGetter> linkCache)
+            where TMod : class, IContextMod<TMod, TModGetter>, TModGetter
+            where TModGetter : class, IModGetter
+            where TMajor : class, IMajorRecord, TMajorGetter
+            where TMajorGetter : class, IMajorRecordGetter
+        {
+            var context = record
+                .ToLinkGetter()
+                .ResolveContext<TMod, TModGetter, TMajor, TMajorGetter>(linkCache)!;
+
+            return new ForwardRecordContext<TMod, TModGetter, TMajor, TMajorGetter>(
+                record,
+                context
+            );
+        }
+
+        public static PatchingData<TMod, TModGetter, TMajor, TMajorGetter, TValue> WithPatchingData<
+            TMod,
+            TModGetter,
+            TMajor,
+            TMajorGetter,
+            TValue
+        >(this IModContext<TMod, TModGetter, TMajor, TMajorGetter> context, TValue data)
+            where TMod : IMod, TModGetter
+            where TModGetter : IModGetter
+            where TMajor : TMajorGetter
+            where TMajorGetter : IMajorRecordQueryableGetter
+            where TValue : notnull => new(context, data);
+    }
 }
