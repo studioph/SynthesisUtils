ed1f04e0988332c042d26acf4352c183e7853769
Paul Hutchings <dev@studiop.page>
2024-10-27 00:25:02 -0400

    Rename quest pipelines

--- Synthesis.Util/Quest/Quest.cs
+++ Synthesis.Util/Quest/Quest.cs
@@ -2,8 +2,17 @@ using Mutagen.Bethesda.Skyrim;
 
 namespace Synthesis.Util.Quest
 {
-    public class QuestPatcherPipeline(ISkyrimMod patchMod)
+    public class QuestForwarderPipeline(ISkyrimMod patchMod)
         : ForwardPatcherPipeline<ISkyrimMod, ISkyrimModGetter, IQuest, IQuestGetter>(patchMod)
+    {
+        protected override void Update(IQuestGetter quest) =>
+            Console.WriteLine($"Patched QUST:{quest.FormKey}({quest.EditorID})");
+    }
+
+    public class QuestPatcherPipeline(ISkyrimMod patchMod)
+        : ConditionalTransformPatcherPipeline<ISkyrimMod, ISkyrimModGetter, IQuest, IQuestGetter>(
+            patchMod
+        )
     {
         protected override void Update(IQuestGetter quest) =>
             Console.WriteLine($"Patched QUST:{quest.FormKey}({quest.EditorID})");
