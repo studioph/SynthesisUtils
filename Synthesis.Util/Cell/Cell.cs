1f1a598c3857a2a7f47dc3245e587288a7717278
Paul Hutchings <dev@studiop.page>
2024-10-27 21:40:37 -0400

    Update cell module

--- Synthesis.Util/Cell/Cell.cs
+++ Synthesis.Util/Cell/Cell.cs
@@ -2,19 +2,10 @@ using Mutagen.Bethesda.Skyrim;
 
 namespace Synthesis.Util.Cell
 {
-    /// <summary>
-    /// Generic interface for forwarding Cell properties
-    /// </summary>
-    public interface ICellForwardPatcher<TValue> : IForwardPatcher<ICell, ICellGetter, TValue>
-        where TValue : notnull { }
-
-    public class CellPatcherPipeline<TValue>(ISkyrimMod patchMod)
-        : ForwardPatcherPipeline<ISkyrimMod, ISkyrimModGetter, ICell, ICellGetter, TValue>(patchMod)
-        where TValue : notnull
+    public class CellForwarderPipeline(ISkyrimMod patchMod)
+        : ForwardPatcherPipeline<ISkyrimMod, ISkyrimModGetter, ICell, ICellGetter>(patchMod)
     {
-        protected override void Update(ICellGetter cell)
-        {
+        protected override void Update(ICellGetter cell) =>
             Console.WriteLine($"Patched CELL:{cell.FormKey}({cell.EditorID})");
-        }
     }
 }
