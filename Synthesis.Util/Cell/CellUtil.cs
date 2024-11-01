e0836e6401558f88661fccf2882679ad3fc3d574
Paul Hutchings <dev@studiop.page>
2024-11-01 13:12:53 -0400

    Add cell getter helpers

--- Synthesis.Util/Cell/CellUtil.cs
+++ Synthesis.Util/Cell/CellUtil.cs
@@ -14,9 +14,36 @@ namespace Synthesis.Util.Cell
         /// </summary>
         /// <param name="cellContext">The cell to check</param>
         /// <returns>True if the cell is a worldspace cell</returns>
-        public static bool IsWorldspaceCell(this IModContext<ICellGetter> cellContext)
-        {
-            return cellContext.TryGetParent<IWorldspaceGetter>(out _);
-        }
+        public static bool IsWorldspaceCell(this IModContext<ICellGetter> cellContext) =>
+            cellContext.TryGetParent<IWorldspaceGetter>(out _);
+
+        /// <summary>
+        /// Retrieves all interior cells from a mod
+        /// </summary>
+        /// <param name="mod">The mod to retrieve records from</param>
+        /// <returns>Cell records not from worldspaces</returns>
+        public static IEnumerable<ICellGetter> GetInteriorCells(this ISkyrimModGetter mod) =>
+            mod
+                .Cells.Records.SelectMany(cellBlock => cellBlock.SubBlocks)
+                .SelectMany(subBlock => subBlock.Cells);
+
+        /// <summary>
+        /// Retrieves all exterior worldspace cells from a mod
+        /// </summary>
+        /// <param name="mod">The mod to retrieve records from</param>
+        /// <returns>The cell records from worldspaces</returns>
+        public static IEnumerable<ICellGetter> GetWorldspaceCells(this ISkyrimModGetter mod) =>
+            mod
+                .Worldspaces.Records.SelectMany(worldspace => worldspace.SubCells)
+                .SelectMany(worldspaceBlock => worldspaceBlock.Items)
+                .SelectMany(worldspaceSubBlock => worldspaceSubBlock.Items);
+
+        /// <summary>
+        /// Retrieves *all* cells from a mod, both interior and exterior worldspace cells
+        /// </summary>
+        /// <param name="mod">The mod to retrieve records from</param>
+        /// <returns>All cell records from the mod</returns>
+        public static IEnumerable<ICellGetter> GetAllCells(this ISkyrimModGetter mod) =>
+            mod.GetInteriorCells().Concat(mod.GetWorldspaceCells());
     }
 }
